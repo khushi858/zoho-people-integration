f77ea1d4854de51de13c38bbed98051b4caef8c6
Khushi Upadhyaya <khushiupadhyay6829@gmail.com>
2026-01-14 11:58:03 +0530

    Fix Zoho People update: use POST instead of PUT

--- insuranceToZoho.js
+++ insuranceToZoho.js
@@ -4,9 +4,11 @@ const { getAccessToken } = require("./token");
 async function updateEmployeeInZoho(recordId, data) {
   const accessToken = await getAccessToken();
 
-  await axios.put(
+  await axios.post(
     `https://people.zoho.in/people/api/forms/employee/records/${recordId}`,
-    { data },
+    {
+      data: data,
+    },
     {
       headers: {
         Authorization: `Zoho-oauthtoken ${accessToken}`,

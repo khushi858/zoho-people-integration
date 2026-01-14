ba40f1cb55b9db3c2c798c4ede758b5a3d686692
Khushi Upadhyaya <khushiupadhyay6829@gmail.com>
2026-01-14 11:40:22 +0530

    Fix Zoho People update payload with data wrapper

--- insuranceToZoho.js
+++ insuranceToZoho.js
@@ -1,12 +1,16 @@
 const axios = require("axios");
 const { getAccessToken } = require("./token");
 
-async function updateEmployeeInZoho(employeeId, data) {
+async function updateEmployeeInZoho(recordId, insuranceData) {
   const accessToken = await getAccessToken();
 
+  const payload = {
+    data: insuranceData,
+  };
+
   await axios.put(
-    `https://people.zoho.in/people/api/forms/P_Employee/records/${employeeId}`,
-    data,
+    `https://people.zoho.in/people/api/forms/P_EmployeeView/records/${recordId}`,
+    payload,
     {
       headers: {
         Authorization: `Zoho-oauthtoken ${accessToken}`,
@@ -14,8 +18,6 @@ async function updateEmployeeInZoho(employeeId, data) {
       },
     }
   );
-
-  console.log("✅ Zoho People updated for employee:", employeeId);
 }
 
 module.exports = { updateEmployeeInZoho };
